43811133d66f39e8581ca8e8a1a347f73ae863e9
Erik de Jonge <erik@a8.nl>
2015-06-16 15:59:32 +0200

    Peter Drucker: Nothing that is Gods is obtainable by money. Tuesday 16 June 2015 (week:24 day:167), 15:59:30

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='unittester',
-      version='48',
+      version='49',
       description='Run python unit-tests as command-line applications (class and method based).',
       url='https://github.com/erikdejonge/unittester',
       author='Erik de Jonge',
       author_email='erik@a8.nl',
       license='GPL',
-      packages=[b'unittester'],
+      packages=['unittester'],
       zip_safe=True,
       #install_requires=['consoleprinter', 'pyprofiler'],
       classifiers=[
