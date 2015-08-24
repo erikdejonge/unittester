01464ca55a2278e6060114c89099cd09434827da
Erik de Jonge <erik@a8.nl>
2015-08-24 21:32:21 +0200

    Monday 24 August 2015 (week:34 day:236), 21:32:17

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='unittester',
-      version='58',
+      version='59',
       description='Run python unit-tests as command-line applications (class and method based).',
       url='https://github.com/erikdejonge/unittester',
       author='Erik de Jonge',
